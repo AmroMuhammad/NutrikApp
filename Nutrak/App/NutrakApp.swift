5014207029a2850fba62104fb92f8411db3d7dc4
Amr Muhammad <amrmu94@gmail.com>
2025-03-29 00:26:45 +0200

    init project

---
+++ Nutrak/App/NutrakApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutrakApp.swift
+//  Nutrak
+//
+//  Created by Amr Muhammad on 29/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct NutrakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
